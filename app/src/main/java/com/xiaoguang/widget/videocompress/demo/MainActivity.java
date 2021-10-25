ec2cf3abe61a0590ece05b59bc63ed6f2c5ba609
hxg <hxg@hande.com>
2021-10-25 11:58:18 +0800

    视频压缩首次提交

---
+++ app/src/main/java/com/xiaoguang/widget/videocompress/demo/MainActivity.java
@@ -0,0 +1,128 @@
+package com.xiaoguang.widget.videocompress.demo;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.xiaoguang.widget.filepicker.FilePickerBuilder;
+import com.xiaoguang.widget.filepicker.FilePickerConst;
+import com.xiaoguang.widget.filepicker.models.sort.SortingTypes;
+import com.xiaoguang.widget.videocompress.dialog.VideoCompressDialog;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_PERMISSION_CAMERA = 10011;
+
+    private static final int REQUEST_CODE_FILE = 10;//文件
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.bt_select_file).setOnClickListener(v ->
+                initPermission());
+    }
+
+    private void initPermission() {
+        //检查相机权限
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                //没有相机权限
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_PERMISSION_CAMERA);
+            } else {
+                filePicker();
+            }
+        } else {
+            filePicker();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_PERMISSION_CAMERA:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted 授予权限
+                    //用户同意了权限申请
+                    filePicker();
+                } else {
+                    // Permission Denied 权限被拒绝
+                    Toast.makeText(this, "初始化文件选择器失败,读取文件权限被拒绝", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    }
+
+    private void filePicker() {
+        String[] zips = {"zip", "rar"};
+        String[] doc = {"doc", "docx"};
+        String[] ppt = {"ppt", "pptx"};
+        String[] pdf = {"pdf"};
+        String[] txt = {"txt"};
+        String[] apk = {"apk"};
+        String[] xls = {"xls", "xlsx"};
+        String[] music = {"m3u", "m4a", "m4b", "m4p", "ogg", "wma", "wmv", "ogg", "rmvb", "mp2", "mp3", "aac", "awb", "amr", "mka"};
+        FilePickerBuilder.getInstance()
+                .setMaxCount(1)
+//                .setSelectedFiles(docPaths)
+//                .setActivityTheme(R.style.DarkTheme2)
+                .enableCameraSupport(false)
+                .showPic(true)
+                .showVideo(true)
+                .enableDocSupport(false)
+                .addFileSupport("Word", doc, R.drawable.ic_file_word)
+                .addFileSupport("压缩包", zips, R.drawable.ic_file_zip)
+                .addFileSupport("PDF", pdf, R.drawable.ic_file_pdf)
+                .addFileSupport("Txt文本", txt, R.drawable.ic_file_txt)
+                .addFileSupport("PPT", ppt, R.drawable.ic_file_ppt)
+                .addFileSupport("安装包", apk, R.drawable.ic_file_zip)
+                .addFileSupport("Excel表格", xls, R.drawable.ic_file_excel)
+                .addFileSupport("音乐", music, R.drawable.ic_file_music)
+                .setActivityTitle("请选择文件")
+                .sortDocumentsBy(SortingTypes.name)
+                .withOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)
+                .pickFile(this, REQUEST_CODE_FILE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_CODE_FILE) {//选择文件
+                ArrayList<String> filePaths = data.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_DOCS);
+                String path = filePaths.get(0);
+//                Toast.makeText(this, "地址： " + path, Toast.LENGTH_SHORT).show();
+                compress(path);
+            }
+        }
+    }
+
+    private void compress(String filePath) {
+        VideoCompressDialog dialog = new VideoCompressDialog(this);
+        dialog.setInputPath(filePath);
+        dialog.setCallback(new VideoCompressDialog.OnCallback() {
+
+            @Override
+            public void success(String successPath) {
+                Toast.makeText(MainActivity.this, "成功，path： " + successPath, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void fail(int code, String message) {
+                Toast.makeText(MainActivity.this, "失败，message： " + message, Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialog.show();
+    }
+}
---
+++ videocompress/src/main/java/com/xiaoguang/widget/videocompress/dialog/VideoCompressDialog.java
@@ -0,0 +1,151 @@
+package com.xiaoguang.widget.videocompress.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.xiaoguang.widget.videocompress.R;
+import com.xiaoguang.widget.videocompress.core.VideoCompress;
+
+import java.io.File;
+import java.text.DecimalFormat;
+
+/**
+ * 视频压缩工具
+ * use：
+ * VideoCompressDialog dialog = new VideoCompressDialog(getActivity());
+ * dialog.setInputPath(filePaths.get(0));
+ * dialog.setCallback(new VideoCompressDialog.OnCallback() {
+ *
+ * @Override public void success(String successPath) {
+ * }
+ * @Override public void fail(int code, String message) {
+ * }
+ * });
+ * dialog.show();
+ * hxg 2021/3/31 17:22 qq:929842234
+ */
+public class VideoCompressDialog extends Dialog {
+
+    private OnCallback callback;
+
+    public interface OnCallback {
+        void success(String successPath);
+
+        void fail(int code, String message);
+    }
+
+    private Context context;
+
+    private TextView tv_des;
+    private TextView tv_progress;
+    private TextView tv_cancel;
+
+    private String inputPath;
+    private String outputPath;
+    private VideoCompress.VideoCompressTask videoCompressTask;
+
+    public VideoCompressDialog(@NonNull Context context) {
+        super(context, R.style.dialog_default_style);
+        this.context = context;
+        String directory = Environment.getExternalStorageDirectory().getPath() + "/Android/data/" + context.getPackageName() + "/cache/videoCompress";
+        File file = new File(directory);
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        this.outputPath = directory + "/compressedMp4.mp4";
+    }
+
+    public void setInputPath(String inputPath) {
+        this.inputPath = inputPath;
+    }
+
+    public void setCallback(OnCallback callback) {
+        this.callback = callback;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.dialog_media_loading);
+
+        tv_des = findViewById(R.id.tv_des);
+        tv_progress = findViewById(R.id.tv_progress);
+        tv_cancel = findViewById(R.id.tv_cancel);
+        tv_cancel.setOnClickListener(v -> progressDialog(0, "compression cancel"));
+
+        setCanceledOnTouchOutside(false);
+
+        getWindow().setBackgroundDrawableResource(R.color.transparent);
+
+        compress();
+    }
+
+    /**
+     * 压缩
+     */
+    private void compress() {
+        videoCompressTask = VideoCompress.compressVideoLow(inputPath, outputPath, new VideoCompress.CompressListener() {
+            @Override
+            public void onStart() {
+            }
+
+            @Override
+            public void onSuccess() {
+                progressDialog(1, "compression success");
+            }
+
+            @Override
+            public void onFail() {
+                progressDialog(-1, "compression error");
+            }
+
+            @Override
+            public void onProgress(float percent) {
+                Log.i("VideoCompressDialog", "percent " + percent);
+                DecimalFormat decimalFormat = new DecimalFormat("0.0");
+                String percentString = decimalFormat.format(percent);
+                setProgressDialog(percentString);
+            }
+        });
+    }
+
+    @Override
+    public void cancel() {
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (null != videoCompressTask) {
+            videoCompressTask.cancel(true);
+        }
+    }
+
+    private void progressDialog(int code, String s) {
+        Log.i("VideoCompressDialog", "code " + code + "  ,msg " + s);
+        if (null != callback) {
+            switch (code) {
+                case -1:
+                    callback.fail(-1, this.getContext().getString(R.string.compress_error));
+                    break;
+                case 0:
+                    callback.fail(0, this.getContext().getString(R.string.compress_cancel));
+                    break;
+                case 1:
+                    callback.success(outputPath);
+                    break;
+            }
+        }
+        dismiss();
+    }
+
+    private void setProgressDialog(String percentString) {
+        tv_progress.setText(percentString);
+    }
+}
